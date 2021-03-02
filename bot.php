f046cd6f66699b63ef2eaad6607c9ebbba91c8d0
Adarsh Kumar <64361893+Bihari420@users.noreply.github.com>
2021-03-02 10:08:22 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ $channel_id = "-100xxxxxxxxxx";
 
     //Extact match Commands
     if($message == "/start"){
-        send_message($chat_id, "Hey $firstname I am $bot_name \nSupport Group - @world_bots \nUse /cmds to view commands \nBot developed by @reboot13 ");
+        send_message($chat_id, "Hey $firstname I am $bot_name \nUse /cmds to view commands \nBot developed by @ChevroletImpala67 ");
     }
 
     if($message == "/cmds"){
@@ -22,15 +22,16 @@ $channel_id = "-100xxxxxxxxxx";
           /search <input> (Google search)
           \n/syt <query> (Youtube Search)
           \n/dict <word> (Dicitonary)
-          \n/smirror <name> (Search Movies/Series)
-          \n/bin <bin> (Bin Data)
+        
+         
+	  \n/bin <bin> (Bin Data)
           \n/weather <name of your city> (Current weather Status)
           \n/dice (random 1-6)
           \n/date (today's date)
           \n/time (current time)
           \n/git <username>
           \n/info (User Info)
-          \n/donate (Donate to Creator)
+          
           ");
     }
 
@@ -69,15 +70,7 @@ if (strpos($message, "/search") === 0) {
     }
   }
 
-//World Mirror Search
-if (strpos($message, "/smirror") === 0) {
-$smovie = substr($message, 9);
-$smovie = preg_replace('/\s+/', '+', $smovie);
-$murl = "[Results-Go to World Mirror](https://witcher.lalbaake456.workers.dev/0:search?q=$smovie)";
-if ($smovie != null) {
-  send_MDmessage($chat_id, $murl);
-}
-}
+
 
 //Youtube Search
 if (strpos($message, "/syt") === 0) {
@@ -90,66 +83,10 @@ if ($syt != null) {
 }
 
 
-///Channel BroadCast
-if (strpos($message, "/broadcast") === 0) {
-$broadcast = substr($message, 11);
-// id == (admins user id)
-if ($id == 1171876903 || $id == 1478297206 || $id == 654455829 || $id == 638178378 ) {
-  send_Cmessage($channel_id, $broadcast);
-}
-}
 
 
-//Bin Lookup
-   if(strpos($message, "/bin") === 0){
-        $bin = substr($message, 5);
-   $curl = curl_init();
-   curl_setopt_array($curl, [
-	CURLOPT_URL => "https://lookup.binlist.net/".$bin,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"authority: lookup.binlist.net",
-		"accept: application/json",
-		"accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
-		"origin: https://binlist.net",
-		"https://binlist.net/",
-		"sec-fetch-dest: empty",
-		"sec-fetch-site: same-site"
-	],
-]);
 
-$result = curl_exec($curl);
-curl_close($curl);
-$data = json_decode($result, true);
-$bank = $data['bank']['name'];
-$country = $data['country']['alpha2'];
-$currency = $data['country']['currency'];
-$emoji = $data['country']['emoji'];
-$scheme = $data['scheme'];
-$Brand = $data['brand'];
-$type = $data['type'];
-  if ($scheme != null) {
-        send_message($chat_id, "
-    Bin: $bin
-Type: $scheme
-Brand : $Brand
-Bank: $bank
-Country: $country $emoji
-Currency: $currency
-Credit/Debit:$type
-Checked By @$username ");
-    }
-else {
-    send_message($chat_id, "Enter Valid BIN");
-}
-   }
-    
+
 
     //Wheather API
 if(strpos($message, "/weather") === 0){
@@ -350,14 +287,14 @@ send_MDmessage($chat_id, "***USD = $inbtc \nBTC = $outvalue \nValue checked by @
 
      ///Send Message (Global)
     function send_message($chat_id, $message){
-        $apiToken =  "API_TOKEN";
+        $apiToken =  "1589653664:AAFB8vOxt4s5Ic7e2DrC8iS-bcCq2HtHEZs";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text");
     }
     
 //Send Messages with Markdown (Global)
       function send_MDmessage($chat_id, $message){
-       $apiToken =  "API_TOKEN";
+       $apiToken =  "1589653664:AAFB8vOxt4s5Ic7e2DrC8iS-bcCq2HtHEZs";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text&parse_mode=Markdown");
     }
@@ -365,13 +302,13 @@ send_MDmessage($chat_id, "***USD = $inbtc \nBTC = $outvalue \nValue checked by @
 
 ///Send Message to Channel
       function send_Cmessage($channel_id, $message){
-       $apiToken =  "API_TOKEN";
+       $apiToken =  "1589653664:AAFB8vOxt4s5Ic7e2DrC8iS-bcCq2HtHEZs";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$channel_id&text=$text");
     }
 
  function sendDice($chat_id, $message){
-       $apiToken =  "API_TOKEN";
+       $apiToken =  "1589653664:AAFB8vOxt4s5Ic7e2DrC8iS-bcCq2HtHEZs";
         file_get_contents("https://api.telegram.org/bot$apiToken/sendDice?chat_id=$chat_id&emoji=$message");
     }
 ?>
